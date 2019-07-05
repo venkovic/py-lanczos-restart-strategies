a379eb8d08d31852bdf01363e49ed6d633b90065
Unknown <nvenkov1@jhu.edu>
2019-07-05 14:15:49 +0200

    first-commit

--- examples/example01.py
+++ examples/example01.py
@@ -15,10 +15,7 @@ n = 1000
 A = np.random.rand(n, n)
 A = A.dot(A.T)
 
-v = np.random.rand(n)
-v /= np.linalg.norm(v)
-
-m = 100
+m = 50
 npairs = 10
 
 eigvals = np.linalg.eigvalsh(A)[-1::-1]
@@ -55,26 +52,29 @@ fig, ax = pl.subplots(1, 3, figsize=(11,3.4), sharey="row")
 lw = .5
 ax[0].set_title("No reorthogonalization")
 for i in range(mj):
-  ax[0].semilogy(approx_eigvals[i,:], "+")
+  ax[0].semilogy(range(1,m), approx_eigvals[i,:], "+")
 for i in range(mj):
-  ax[0].semilogy(approx_eigvals[i,:], "-", lw=lw)
+  ax[0].semilogy(range(1,m), approx_eigvals[i,:], "-", lw=lw)
 ax[0].semilogy(eigvals.shape[0]*[mj+3], eigvals, "k_", lw=0)
 
 ax[1].set_title("Full reorthogonalization")
 for i in range(mj):
-  ax[1].semilogy(approx_eigvals_full[i,:], "+")
+  ax[1].semilogy(range(1,m), approx_eigvals_full[i,:], "+")
 for i in range(mj):
-  ax[1].semilogy(approx_eigvals_full[i,:], "-", lw=lw)
+  ax[1].semilogy(range(1,m), approx_eigvals_full[i,:], "-", lw=lw)
 ax[1].semilogy(eigvals.shape[0]*[mj+3], eigvals, "k_", lw=0)
 
 ax[2].set_title("Selective reorthogonalization")
 for i in range(mj):
-  ax[2].semilogy(approx_eigvals_selective[i,:], "+")
+  ax[2].semilogy(range(1,m), approx_eigvals_selective[i,:], "+")
 for i in range(mj):
-  ax[2].semilogy(approx_eigvals_selective[i,:], "-",  lw=lw)
+  ax[2].semilogy(range(1,m), approx_eigvals_selective[i,:], "-",  lw=lw)
+for j in range(3):
+  ax[j].grid()
 ax[2].semilogy(eigvals.shape[0]*[mj+3], eigvals, "k_", lw=0)
 ax[1].set_ylim(ax[0].get_ylim()); ax[2].set_ylim(ax[0].get_ylim())
 ax[0].set_ylabel("Approximate eigenvalues")
+fig.suptitle(r"$\{\lambda_i(T_m)\}_{i=1}^m$")
 ax[0].set_xlabel("m"); ax[1].set_xlabel("m"); ax[2].set_xlabel("m")
 pl.savefig(figures_path+"example01_lanczos_a.png", bbox_inches='tight')
 #pl.show()
@@ -84,89 +84,49 @@ lw = .8
 ax[0].set_title("No reorthogonalization")
 for i in range(mj):
   ind = (iterated_error_bound[i,:]==None).sum()
-  ax[0].semilogy(np.concatenate((iterated_error_bound[i,:ind],iterated_error_bound[i,ind:]/approx_eigvals[i,ind:])), "-", lw=lw)
+  ax[0].semilogy(range(1,m), np.concatenate((iterated_error_bound[i,:ind],iterated_error_bound[i,ind:]/approx_eigvals[i,ind:])), "-", lw=lw)
 ax[1].set_title("Full reorthogonalization")
 for i in range(mj):
   ind = (iterated_error_bound_full[i,:]==None).sum()
-  ax[1].semilogy(np.concatenate((iterated_error_bound_full[i,:ind],iterated_error_bound_full[i,ind:]/approx_eigvals_full[i,ind:])), "-", lw=lw)
+  ax[1].semilogy(range(1,m), np.concatenate((iterated_error_bound_full[i,:ind],iterated_error_bound_full[i,ind:]/approx_eigvals_full[i,ind:])), "-", lw=lw)
 ax[2].set_title("Selective reorthogonalization")
 for i in range(mj):
   ind = (iterated_error_bound_selective[i,:]==None).sum()
-  ax[2].semilogy(np.concatenate((iterated_error_bound_selective[i,:ind],iterated_error_bound_selective[i,ind:]/approx_eigvals_selective[i,ind:])), "-", lw=lw)
+  ax[2].semilogy(range(1,m), np.concatenate((iterated_error_bound_selective[i,:ind],iterated_error_bound_selective[i,ind:]/approx_eigvals_selective[i,ind:])), "-", lw=lw)
 ax[1].set_ylim(ax[0].get_ylim()); ax[2].set_ylim(ax[0].get_ylim())
 ax[0].set_ylim(1e-16,1e2)
 ax[0].set_ylabel("Iterated relative error bound")
+fig.suptitle(r"$\{|\beta_mS_{mi}/\lambda_i(T_m)|\}_{i=1}^m$")
 for j in range(3):
   ax[j].set_xlabel("m")
   ax[j].grid()
 pl.savefig(figures_path+"example01_lanczos_b.png", bbox_inches='tight')
 #pl.show()
 
+fig, ax = pl.subplots(1, 3, figsize=(11,3.4), sharey="row")
+lw = .8
+ax[0].set_title("No reorthogonalization")
+for i in range(mj):
+  ind = (approx_eigvals[i,:]==None).sum()
+  ax[0].semilogy(range(1,m), np.concatenate((approx_eigvals[i,:ind],np.abs((approx_eigvals[i,ind:]-eigvals[i])/eigvals[i]))), "-", lw=lw)
+ax[1].set_title("Full reorthogonalization")
+for i in range(mj):
+  ind = (approx_eigvals_full[i,:]==None).sum()
+  ax[1].semilogy(range(1,m), np.concatenate((approx_eigvals_full[i,:ind],np.abs((approx_eigvals_full[i,ind:]-eigvals[i])/eigvals[i]))), "-", lw=lw)
+ax[2].set_title("Selective reorthogonalization")
+for i in range(mj):
+  ind = (approx_eigvals_selective[i,:]==None).sum()
+  ax[2].semilogy(range(1,m), np.concatenate((approx_eigvals_selective[i,:ind],np.abs((approx_eigvals_selective[i,ind:]-eigvals[i])/eigvals[i]))), "-", lw=lw)
+ax[0].set_ylabel("Global error")
+fig.suptitle("Global error, "+r"$\{|\lambda_i(T_m)-\lambda_i(A)|/|\lambda_i(A)|\}_{i=1}^m$")
+for j in range(3):
+  ax[j].set_xlabel("m")
+  ax[j].grid()
+pl.savefig(figures_path+"example01_lanczos_c.png", bbox_inches='tight')
+#pl.show()
 
-"""
-V, alpha, beta = Lanczos(A, v, m)
-
-n_eigvecs = 7
-eigvals = get_eigvals(A, n_eigvecs)
-err_bnd, iterated_err_bnd, approx_eigvals, global_err, local_err = [], [], [], [], []
-for m_j in range(10, m+1):
-  T = get_T(alpha[:m_j], beta[:m_j-1])
-  Y, _approx_eigvals, _err_bnd, _iterated_err_bnd, _global_err, _local_err = get_approx_eigvecs(T, V[:,:m_j], A, beta[:m_j-1], n_eigvecs, eigvals=eigvals, output_format=1)
-  approx_eigvals += [_approx_eigvals]
-  err_bnd += [_err_bnd]
-  iterated_err_bnd += [_iterated_err_bnd]
-  global_err += [_global_err]
-  local_err += [_local_err]
-err_bnd, iterated_err_bnd = np.array(err_bnd), np.array(iterated_err_bnd)
-global_err, local_err = np.array(global_err), np.array(local_err)
-
-fig = pl.figure()
-for i in range(n_eigvecs):
-  pl.semilogy([_eigvals[i] for _eigvals in approx_eigvals])
-  pl.xlabel("m")
-  pl.ylabel(r"$\theta_k^{(m)}$")
-pl.show()
-"""
-
-
-"""
-err_, iterated_err_bnd, approx_eigvals, global_err, local_err = [], [], [], [], []
-for m_j in range(10, m+1):
-  T = get_T(alpha[:m_j], beta[:m_j-1])
-  Y, _approx_eigvals, _err_bnd, _iterated_err_bnd, _global_err, _local_err = get_approx_eigvecs(T, V[:,:m_j], A, beta[:m_j-1], n_eigvecs, eigvals=eigvals, output_format=1)
-  approx_eigvals += [_approx_eigvals]
-  err_bnd += [_err_bnd]
-  iterated_err_bnd += [_iterated_err_bnd]
-  global_err += [_global_err]
-  local_err += [_local_err]
-err_bnd, iterated_err_bnd = np.array(err_bnd), np.array(iterated_err_bnd)
-global_err, local_err = np.array(global_err), np.array(local_err)
-
-fig = pl.figure()
-for i in range(n_eigvecs):
-  pl.semilogy([_eigvals[i] for _eigvals in approx_eigvals])
-  pl.xlabel("m")
-  pl.ylabel(r"$\theta_k^{(m)}$")
-pl.show()
-
-for i in range(n_eigvecs):
-  fig = pl.figure()
-  pl.semilogy(err_bnd[:,i], label=r"$\|Ay_{%d}^{(m)}-\theta_{%d}^{(m)}y_{%d}^{(m)}\|_2/|\lambda_{%d}(A)|$" %(i+1, i+1, i+1, i+1))
-  pl.semilogy(iterated_err_bnd[:,i], label=r"$|\beta_m||(S_m)_{m,%d}|/|\lambda_{%d}(A)|$" %(i+1, i+1))
-  pl.semilogy(global_err[:,i], label=r"$|\theta_{%d}^{(m)}-\lambda_{%d}(A)|/|\lambda_{%d}(A)|$" %(i+1, i+1, i+1))
-  pl.semilogy(local_err[:,i], label=r"$\min_{j}|\theta_{%d}^{(m)}-\lambda_{j}(A)|/|\lambda_{%d}(A)|$" %(i+1, i+1))
-  pl.xlabel("m")
-  pl.ylabel("Error bound and iterated error bound")
-  pl.legend()
-  pl.show()
-"""
-
-# Q1:    HOW DO YOU ASSESS THE ACCURACY OF THOSE APPROXIMATION.
-#        i.e. HOW DO YOU PICK m for a given n_eigvecs
+# TO DO:
+# Q1:    HOW DO YOU PICK m for a given n_eigvecs?
 #        You can not know this, see p. 239-240 of Dongara et al.'s book.
-# R1/T1: See oscillations of error bounds (iterated or not).
-#        Plot local/global errors as in p. 370-371 of Demmel's book.
-#        Try
 # T2:    Implement restart strategies.
 #        Explicit vs implicit, see p. 239-240 of Dongara et al.'s book
-# T3:    Measure and plot LOO?
--- lanczos.py
+++ lanczos.py
@@ -2,11 +2,6 @@ import numpy as np
 import scipy.sparse as sparse
 import scipy.sparse.linalg
 
-# See https://people.eecs.berkeley.edu/~demmel/ma221_Fall09/Matlab/
-#     https://people.eecs.berkeley.edu/~demmel/ma221_Fall09/Matlab/LANCZOS_README.html
-
-
-
 def lanczos(A, npairs, m=50, q1=None, reortho=None, eigvals=None):
   # Implementation #1 : Algo. 7.1 (Demmel, 1997);
   # (  we use this )    Algo. 10.1.1 (Golub, 2012)
@@ -38,8 +33,6 @@ def lanczos(A, npairs, m=50, q1=None, reortho=None, eigvals=None):
   Q[:,0] = q1
   
   if isinstance(eigvals, np.ndarray):
-    #data = {"approx_eigvecs":[], "approx_eigvals":[], "rel_iterated_error_bound":[], \
-    #    "rel_error_bound":[], "global_error":[], "local_error":[]}
     data = {"approx_eigvecs":[], "approx_eigvals":[], "iterated_error_bound":[]}
 
   for j in range(m):
@@ -78,57 +71,7 @@ def lanczos(A, npairs, m=50, q1=None, reortho=None, eigvals=None):
       data["approx_eigvals"] += [theta[-1::-1]]
       data["iterated_error_bound"] += [np.abs(beta[j+1]*S[j,-1::-1])]
 
-      # data["rel_error_bound"], data["global_error"] and data["local_error"]
-      # can be computed after
-
-
-      # Get iterated error bounds, ...
-
-      """
-      if not isinstance(eigvals, np.ndarray):
-        approx_eigvecs, approx_eigvals, iterated_error_bound = \
-          get_approx_eigvecs(V[:,:j+1], A, alpha[:j+1], beta[1:j+1], beta[j+1], j+1)
-      else:
-        approx_eigvecs, approx_eigvals, rel_iterated_error_bound, \
-        rel_error_bound, global_error, local_error = \
-          get_approx_eigvecs(V[:,:j+1], alpha[:j+1], beta[1:j+1], beta[j+1], j+1, A=A, eigvals=eigvals)
-        data["approx_eigvecs"] += [approx_eigvecs]
-        data["approx_eigvals"] += [approx_eigvals]
-        data["rel_iterated_error_bound"] += [rel_iterated_error_bound]
-        data["rel_error_bound"] += [rel_error_bound]
-        data["global_error"] += [global_error]
-        data["local_error"] += [local_error]
-      """
-
   if not isinstance(eigvals, np.ndarray):
     return approx_eigvecs, approx_eigvals, iterated_error_bound
   else:
     return data
-
-def get_approx_eigvecs(V, alpha, beta, beta_j, n_eigvecs, A=None, eigvals=None):
-  reduced_eigvals, reduced_eigvecs = scipy.linalg.eigh_tridiagonal(alpha, beta, select="a")
-  reduced_eigvals = reduced_eigvals[-1::-1]
-  reduced_eigvecs = reduced_eigvecs[:,-1::-1]
-  # reduced eigpairs are sorted from most to less dominant
-
-  # Form Ritz approximate eigenvectors of A
-  Y = V.dot(reduced_eigvecs)
-
-  # Iterated error bound:
-  iterated_error_bound = [abs(beta_j)*abs(reduced_eigvecs[-1,i]) for i in range(n_eigvecs)]
-  
-  if not isinstance(eigvals, np.ndarray):
-    #return Y, reduced_eigvals, iterated_error_bound
-    return Y, reduced_eigvals, iterated_error_bound
-
-  else:
-    rel_iterated_error_bound = np.array(iterated_error_bound)/np.abs(eigvals[:n_eigvecs])
-
-    # Exact error bound:
-    error_bound = [np.linalg.norm(A.dot(Y[:,i])-reduced_eigvals[i]*Y[:,i]) for i in range(n_eigvecs)]
-    rel_error_bound = np.array(error_bound)/np.abs(eigvals[:n_eigvecs])
-
-    # GLobal and local errors, see p. 370 in Demmel's textbook:
-    global_error = np.abs(reduced_eigvals-eigvals[:n_eigvecs])/np.abs(eigvals[:n_eigvecs])
-    local_error = np.array([np.min(reduced_eigvals-eigvals[i]) for i in range(n_eigvecs)])/np.abs(eigvals[:n_eigvecs])
-    return Y, reduced_eigvals, rel_iterated_error_bound, rel_error_bound, global_error, local_error
